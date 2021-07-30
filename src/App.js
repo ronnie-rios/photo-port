d659cb3a5ba42c571c08c99987c70354b2bb7294
Ronnie Rios <ronniesmail14@gmail.com>
2021-07-30 12:20:25 -0500

    fixed syntax errors

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
 function App() {
-  const [contactSelected, setContactSelected] = useState(false);
-
   const [categories] = useState([
     {
       name: 'commercial',
@@ -19,24 +17,26 @@ function App() {
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div>
       <Nav
-       categories={categories}
-       setCurrentCategory={setCurrentCategory}
-       currentCategory={currentCategory}
-       contactSelected={contactSelected}
-       setContactSelected={setContactSelected}
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-          {!contactSelected ? (
-        <>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
-         </>
-          ) : (
-        <ContactForm></ContactForm>
-      )}
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
